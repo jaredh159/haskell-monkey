05bad4fa1c3c57f98964d94ec209b42c3f1cf391
Jared Henderson <jaredh159@users.noreply.github.com>
2024-08-01 08:55:16 -0400

    evaluate bool literals and prefix exprs

--- src/Eval.hs
+++ src/Eval.hs
@@ -2,18 +2,33 @@ module Eval (
     eval
   ) where
 
+import Prelude hiding (negate)
 import qualified Ast
 import Object
 
 eval :: Ast.Node -> Object
-eval (Ast.ProgNode prog) = foldl (\_ stmt -> eval (Ast.StmtNode stmt)) ObjNull prog
+eval (Ast.ProgNode []) = ObjNull
+eval (Ast.ProgNode prog) = last $ map (eval . Ast.StmtNode) prog
 eval (Ast.StmtNode stmt) = evalStmt stmt
-eval node = error $ "Node not handled: " ++ show node
+eval (Ast.ExprNode expr) = evalExpr expr
 
 evalStmt :: Ast.Stmt -> Object
 evalStmt (Ast.ExprStmt expr) = evalExpr expr
-evalStmt _ = error "unhandled stmt"
+evalStmt stmt = error  $ "Unhandled statement type: " ++ show stmt
 
 evalExpr :: Ast.Expr -> Object
 evalExpr (Ast.IntLit int) = ObjInt int
-evalExpr expr = error $ "unhandled expr type" ++ show expr
+evalExpr (Ast.BoolLit bool) = ObjBool bool
+evalExpr (Ast.Prefix Ast.PrefixBang expr) = negate $ eval $ Ast.ExprNode expr
+evalExpr (Ast.Prefix Ast.PrefixMinus expr) = case eval (Ast.ExprNode expr) of
+  (ObjInt int) -> ObjInt (-int)
+  _ -> ObjNull -- TODO: error
+evalExpr expr = error $ "Unhandled expr type: " ++ show expr
+
+negate :: Object -> Object
+negate (ObjBool bool) = ObjBool $ not bool
+negate ObjNull = ObjBool True
+negate _ = ObjBool False
+
+
+
--- test/EvalSpec.hs
+++ test/EvalSpec.hs
@@ -9,9 +9,21 @@ import ParserSpec (program)
 
 spec :: Spec
 spec = do
-  it "should evaluate integer expressions" $ do
+  it "should evaluate literal expressions" $ do
     evalp "5" `shouldBe` ObjInt 5
     evalp "10" `shouldBe` ObjInt 10
+    evalp "true" `shouldBe` ObjBool True
+    evalp "false" `shouldBe` ObjBool False
+
+  it "should evaluate prefix expressions" $ do
+    evalp "!true" `shouldBe` ObjBool False
+    evalp "!false" `shouldBe` ObjBool True
+    evalp "!5" `shouldBe` ObjBool False
+    evalp "!!true" `shouldBe` ObjBool True
+    evalp "!!false" `shouldBe` ObjBool False
+    evalp "!!5" `shouldBe` ObjBool True
+    evalp "-5" `shouldBe` ObjInt (-5)
+    evalp "-10" `shouldBe` ObjInt (-10)
 
 -- helpers
 
